9d80507bec76e19dcdefebbe5fbd459c0595ae1a
fabiha khalid <fabihakhalid2005@gmail.com>
2025-01-19 16:15:28 -0800

    largest array

---
+++ largest array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int findLargest(int arr[], int n) {
+    int largest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 100};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Largest element: " << findLargest(arr, n) << endl;
+    return 0;
+}
